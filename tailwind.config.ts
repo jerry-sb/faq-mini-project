c5686fe52549490fd7e7778bf5b08e8491c00460
jerry-sb <mugbo1410@gmail.com>
2025-05-20 22:59:52 +0900

    style: tailwind 관련 리소스 수정 및 추가 - tailwind v3에 맞춘 global.css 수정 - css variable 사용을 위한   - tailwind.config, postcss.config.mjs, util 추가
    style: next fonts를 설정하기 위한 fonts 파일 위치 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,88 @@ export default {
    ],
    prefix: "",
    theme: {
-      extend: {},
+      extend: {
+         top: {
+            header: "var(--header-height)",
+         },
+         inset: {
+            header: "var(--header-height)",
+         },
+         fontSize: {
+            base: "var(--font-size)",
+            tab: "var(--tab-fsize)",
+            heading2: "var(--heading-2)",
+            heading4: "var(--heading-4)",
+            headingInfo: "var(--heading-info)",
+            h1: "var(--h1-fsize)",
+            h1sm: "var(--h1-fsize-sm)",
+            faq: "var(--faq-list-a-size)",
+         },
+         boxShadow: {
+            header: "0 4px 32px 0 rgba(0, 0, 0, 0.08)",
+         },
+         spacing: {
+            xsm: "var(--space-xsm)",
+            xsm2: "var(--space-xsm2)",
+            sm: "var(--space-sm)",
+            sm2: "var(--space-sm2)",
+            md: "var(--space-md)",
+            md2: "var(--space-md2)",
+            md3: "var(--space-md3)",
+            box: "var(--space-box)",
+            box2: "var(--space-box2)",
+            side: "var(--side-padding)",
+            bottom: "var(--bottom-padding)",
+            gallery: "var(--gallery-list-space)",
+            faqQ: "var(--faq-list-q-padding)", // e.g. py-faqQ
+         },
+         height: {
+            btnMd: "var(--btn-md)",
+            btnXlg: "var(--btn-xlg)",
+            btnXlg2: "var(--btn-xlg2)",
+            btnXxlg: "var(--btn-xxlg)",
+            header: "var(--header-height)",
+            footer: "var(--footer-height)",
+            h1: "var(--h1-height)",
+         },
+         width: {
+            searchBar: "var(--search-bar-width)",
+         },
+         maxWidth: {
+            boardMedia: "var(--board-media-max-width)",
+            "max-width": "var(--max-width)",
+         },
+         fontFamily: {
+            kia: ["var(--font-kia)", "Arial", "Helvetica", "sans-serif"],
+         },
+         lineHeight: {
+            header: "var(--header-height)",
+         },
+         keyframes: {
+            "slide-in-left": {
+               from: {
+                  transform: "translateX(-100%)",
+               },
+               to: {
+                  transform: "translateX(0)",
+               },
+            },
+            "slide-out-left": {
+               from: {
+                  transform: "translateX(0)",
+               },
+               to: {
+                  transform: "translateX(-100%)",
+               },
+            },
+         },
+         animation: {
+            "slide-in-left":
+               "slide-in-left 0.7s cubic-bezier(1, 0, 0.2, 1) forwards",
+            "slide-out-left":
+               "slide-out-left 0.7s cubic-bezier(1, 0, 0.2, 1) forwards",
+         },
+      },
    },
    plugins: [animate],
 } satisfies Config;
