75ba43231e90c125a16a88da7909e00f65ae0d65
jerry-sb <mugbo1410@gmail.com>
2025-05-20 23:08:33 +0900

    feat(root-layout): 기본 레이아웃 구조 및 글로벌 폰트 적용

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+
+import localFont from "next/font/local";
+
+const kiaFont = localFont({
+   src: [
+      {
+         path: "./fonts/KiaSignatureFixRegular.woff2",
+         weight: "400",
+      },
+      {
+         path: "./fonts/KiaSignatureFixBold.woff2",
+         weight: "700",
+      },
+   ],
+   variable: "--font-kia",
+});
+
+export default function RootLayout({
+   children,
+}: Readonly<{
+   children: React.ReactNode;
+}>) {
+   return (
+      <html lang="en">
+         <body className={`${kiaFont.variable} font-kia`}>
+            <main>{children}</main>
+         </body>
+      </html>
+   );
+}
