e7dd7129ce61d2b2e96b113536542b01ad8d14f0
jerry-sb <mugbo1410@gmail.com>
2025-05-24 15:48:25 +0900

    fix(ui): ui-info의 컨텐츠 리스트 업로드

---
+++ src/features/common/components/ui/constants/array.ts
@@ -0,0 +1,42 @@
+export const inquiryInfoItems = [
+   {
+      href: "/static/media/proposal.pdf",
+      icon: "/svg/ic_download.svg",
+      title: "서비스 제안서 다운로드",
+      download: true,
+   },
+   {
+      href: "https://wiblebiz.kia.com/Counsel",
+      icon: "/svg/ic_write.svg",
+      title: "상담문의 등록하기",
+   },
+   {
+      href: "https://pf.kakao.com/_xflxjdb",
+      icon: "/svg/ic_talk.svg",
+      title: "카톡으로 문의하기",
+      sub: "ID : 기아 비즈",
+   },
+];
+
+export const processInfoItems = [
+   {
+      icon: "/svg/ic_process01.svg",
+      title: "문의 등록",
+      desc: "상담 문의를 등록해 주시면, 담당자가 맞춤형 상담을 제공합니다.",
+   },
+   {
+      icon: "/svg/ic_process02.svg",
+      title: "관리자 설정",
+      desc: "관리자 Web 접속 후 결제방식 및 회사정보를 설정합니다.",
+   },
+   {
+      icon: "/svg/ic_process03.svg",
+      title: "임직원 가입",
+      desc: "이용자 App에서 회원가입 후 소속 회사 인증을 진행합니다.",
+   },
+   {
+      icon: "/svg/ic_process04.svg",
+      title: "서비스 이용",
+      desc: "이용자 App에서 차량 예약을 하고 K존에서 바로 이용하세요!",
+   },
+];
